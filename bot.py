5b8d112d74804c29cad7c1a20c02c936a4226eea
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-07-17 21:48:49 +0200

    nuevos comandos, bug fixes

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import os
 from os import listdir
 import threading
 import time
+import pytube
 import youtube_dl
 
 from pytube import Search
@@ -17,6 +18,7 @@ cancion = False
 conectado = False
 canciones = []
 
+vc = ""
 
 num = 0
 
@@ -35,26 +37,34 @@ async def on_message(message):
     global conectado
     global num
     global canciones
-    if message.content.startswith("@p"):
+    global vc
+    if message.content.startswith(".p"):
         link = message.content[3: ]
         print(link)
-        s = Search(link)
-        print(s.results)
-        print(type(s.results))
-        t = s.results[0]
-        print(type(t))
-        print(str(t)[41:-2])
-        img = t.thumbnail_url
-        link = f"https://youtu.be/{str(t)[41:-1]}"
-        print(img)
+        if "https" in link:
+           t = pytube.YouTube(link)
+           img = t.thumbnail_url
         
+        else:
+
+            s = Search(link)
+            print(s.results)
+            print(type(s.results))
+            t = s.results[0]
+            print(type(t))
+            print(str(t)[41:-2])
+            img = t.thumbnail_url
+            link = f"https://youtu.be/{str(t)[41:-1]}"
+            print(img)
+            
         canciones.append(link)
         print(canciones[0])
         if cancion == True:
             mensaje1 = discord.Embed(title= f"{t.title}", description= f"Se ha puesto en cola {t.title}, esta en el puesto {len(canciones)}", url=link)
-            await message.channel.send(embed= mensaje1)
             mensaje1.set_thumbnail(url=img)
             mensaje1.set_author(name= message.author.name, icon_url=message.author.avatar_url)
+            await message.channel.send(embed= mensaje1)
+
             print(canciones)
            
             
@@ -82,26 +92,32 @@ async def on_message(message):
                 while True:
                     time.sleep(1)
                     if vc.is_playing() == False:
-                        conectado = False
-                        os.remove(f"{num}.mp4")
-                        
-                        
-                        ydl_opts = {}
-                        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                            
-                            ydl.download([canciones[0]])
-                            
-                        for files in listdir(path="C:/Users/carly/Downloads/bot_musica_discord"):
-                            print(files)
-                            if ".mp4" in files:
-                                mensaje = discord.Embed(title= f"{t.title}", description= f"Se esta reproduciendo {t.title}", url=link)
-                                mensaje.set_thumbnail(url=img)
-                                mensaje.set_author(name= message.author.name, icon_url=message.author.avatar_url)
-                                os.rename(files, f"{num}.mp4")
-                                vc.play(discord.FFmpegPCMAudio(executable=r"C:\Users\carly\Downloads/ffmpeg-4.4-full_build/ffmpeg-4.4-full_build/bin/ffmpeg.exe", source=f"0.mp4"))
 
-                                del canciones[0]
-                                num = 0
+                        try:
+                            conectado = False
+                            os.remove(f"{num}.mp4")
+                        
+                        
+                            ydl_opts = {}
+                            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                                
+                                ydl.download([canciones[0]])
+
+                            
+                            for files in listdir(path="C:/Users/carly/Downloads/bot_musica_discord"):
+                                print(files)
+                                if ".mp4" in files:
+                                    mensaje = discord.Embed(title= f"{t.title}", description= f"Se esta reproduciendo {t.title}", url=link)
+                                    mensaje.set_thumbnail(url=img)
+                                    mensaje.set_author(name= message.author.name, icon_url=message.author.avatar_url)
+                                    os.rename(files, f"{num}.mp4")
+                                    vc.play(discord.FFmpegPCMAudio(executable=r"C:\Users\carly\Downloads/ffmpeg-4.4-full_build/ffmpeg-4.4-full_build/bin/ffmpeg.exe", source=f"0.mp4"))
+                                    del canciones[0]
+                                    num = 0
+                        
+                        except Exception:
+                            pass
+                        
 
                     else:
                         conectado = True
@@ -114,6 +130,38 @@ async def on_message(message):
             await message.channel.send(embed=mensaje)
             del canciones[0]
             num = 0
+    
+    if message.content == ".skip":
+        vc.stop()
+        os.remove(f"{num}.mp4")
+    
+    
+        ydl_opts = {}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            
+            ydl.download([canciones[0]])
+            
+        for files in listdir(path="C:/Users/carly/Downloads/bot_musica_discord"):
+            print(files)
+            if ".mp4" in files:
+                mensaje = discord.Embed(title= f"{t.title}", description= f"Se esta reproduciendo {t.title}", url=link)
+                mensaje.set_thumbnail(url=img)
+                mensaje.set_author(name= message.author.name, icon_url=message.author.avatar_url)
+                await message.channel.send(embed=mensaje)
+                os.rename(files, f"{num}.mp4")
+                vc.play(discord.FFmpegPCMAudio(executable=r"C:\Users\carly\Downloads/ffmpeg-4.4-full_build/ffmpeg-4.4-full_build/bin/ffmpeg.exe", source=f"0.mp4"))
+                del canciones[0]
+                num = 0
+        
+    if message.content == ".calla":
+        vc.pause()
+        await message.channel.send("Vale bro :(")
+    
+    if message.content == ".continua":
+        vc.resume()
+        await message.channel.send("Procedo a continuar 🎶")
+        
+
             
 
 

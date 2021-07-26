fce5a8c4249561baf2d3ee2661c6ac0c9f16c343
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-07-26 23:42:13 +0200

    testing test 6

--- test_bot.py
+++ test_bot.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.mark.parametrize(
     "message, expected1, expected2, expected3, expected4", [
-        (".p https://youtu.be/dQw4w9WgXcQ", True, False, 1, [https://youtu.be/dQw4w9WgXcQ])
+        (".p https://youtu.be/dQw4w9WgXcQ", True, False, 1, ["https://youtu.be/dQw4w9WgXcQ"])
     ]
 )
 def test_con(message):

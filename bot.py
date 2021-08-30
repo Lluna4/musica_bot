e8c225b2f1daa9b5a2f203b7233be3c5315c3a0f
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-08-30 22:52:39 +0200

    xd
    bug fixes

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import time
 
 from pytube import Search
 
-exe = r"C:\Users\carly\Downloads/ffmpeg-4.4-full_build/ffmpeg-4.4-full_build/bin/ffmpeg.exe"
+exe = "/usr/bin/ffmpeg"
 canciones = []
 conectado = False
 vc = ""
@@ -27,6 +27,9 @@ server.bind(ADDR)
 puesto = False
 conn = ""
 sec = 0
+inf2 = ""
+info = ""
+message2 = ""
 
 intents = discord.Intents.default()
 intents.members = True  
@@ -35,7 +38,7 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 async def on_ready():
     print("uff")
-    await bot.change_presence(activity=discord.Game(name="Ser musico"))
+    await bot.change_presence(activity=discord.Game(name="Recordar al mas grande"))
 
 
 @bot.event
@@ -44,12 +47,17 @@ async def on_message(message):
     global vc
     global puesto
     global sec
-    if message.content.startswith(".p"):
+    global inf2
+    global info
+    global message2
+    if message.content.startswith("-p"):
         link = await linkd(message.content)
         if "https" in link:
             info = pytube.YouTube(link)
+            inf2 = link
         else:
             info = await texto_a_link(link)
+            inf2 = await texto_a_linkf(link)
         try:
             canciones.append(link)
             canal = message.author.voice.channel
@@ -61,12 +69,12 @@ async def on_message(message):
             return
         
         if vc.is_playing(): #si hay una cancion reproduciendose lo pone en cola
-           msg = discord.Embed(title= f"{info.title}", description= f"Se ha puesto en cola {info.title}, esta en el puesto {len(canciones) - 1}", url=link)
+           msg = discord.Embed(title= f"{info.title}", description= f"Se ha puesto en cola {info.title}, esta en el puesto {len(canciones) - 1}", url=inf2)
            msg.set_thumbnail(url=info.thumbnail_url)
            msg.set_author(name= message.author.name, icon_url=message.author.avatar_url)
            await message.channel.send(embed=msg)
         else: #reproduce la cancion
-            msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=link)
+            msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=inf2)
             msg.set_thumbnail(url=info.thumbnail_url)
             msg.set_author(name= message.author.name, icon_url=message.author.avatar_url)
             ydl_opts = {}
@@ -77,27 +85,28 @@ async def on_message(message):
             vc.play(discord.FFmpegPCMAudio(executable=exe, source=song_info["formats"][0]["url"], **OP))
             await message.channel.send(embed=msg)
             if puesto == False:
-                x1 = threading.Thread(target=xd)
-                x1.start()
+                asyncio.get_event_loop().create_task(xd())
                 
 
                 puesto = True
     
-    if message.content.startswith(".adelantar"):
+    if message.content.startswith("-adelantar"):
         await adelantar_y_atrasar(message.content, canciones)
     
-    if message.content.startswith(".atrasar"):
+    if message.content.startswith("-atrasar"):
         await atrasar(message.content, canciones)
     
-    if message.content == ".skip":
+    if message.content == "-skip":
         try:
-            vc.stop()
+            
             del canciones[0]
             info = pytube.YouTube(canciones[0])
             msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=canciones[0])
             msg.set_thumbnail(url=info.thumbnail_url)
             msg.set_author(name= message.author.name, icon_url=message.author.avatar_url)
             ydl_opts = {}
+            vc.stop()
+            
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 song_info = ydl.extract_info(canciones[0], download=False)
                 #print(song_info)
@@ -105,14 +114,21 @@ async def on_message(message):
             vc.play(discord.FFmpegPCMAudio(executable=exe, source=song_info["formats"][0]["url"], **OP))
             await message.channel.send(embed=msg)
         except Exception:
+            canciones.append(link)
             await message.channel.send("No hay cancion a la que skipear")
     
-    if message.content == ".esperate":
+    if message.content == "-stop":
         vc.pause()
         await message.channel.send("Vale bro")
-    if message.content == ".sigue":
+    if message.content == "-resume":
         vc.resume()
         await message.channel.send("Volviendo con la musica")
+    if message.content == "-disconnect":
+        vc.disconnect()
+    
+
+    
+    
     
 
 
@@ -122,29 +138,34 @@ async def on_message(message):
         
 
         
-def xd():
+async def xd():
     global sec
+    global message2
     sec = 0
     
     while True:
+        try:
 
         
-        if vc.is_playing() == False and vc.is_paused() == False:
-            
-            time.sleep(1)
-            if vc.is_playing() == False:
-                sec = 0
-                del canciones[0]
-                info = pytube.YouTube(canciones[0])
-                msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=canciones[0])
-                msg.set_thumbnail(url=info.thumbnail_url)
-                #msg.set_author(name= message.author.name, icon_url=message.author.avatar_url) #hay que guardar autores para que esto funcione
-                ydl_opts = {}
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    song_info = ydl.extract_info(canciones[0], download=False)
-                    #print(song_info)
-                OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-                vc.play(discord.FFmpegPCMAudio(executable=exe, source=song_info["formats"][0]["url"], **OP))
+            if vc.is_playing() == False and vc.is_paused() == False:
+                
+                time.sleep(1)
+                if vc.is_playing() == False:
+                    sec = 0
+                    del canciones[0]
+                    info = pytube.YouTube(canciones[0])
+                    msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=canciones[0])
+                    msg.set_thumbnail(url=info.thumbnail_url)
+                    msg.set_author(name= message2.author.name, icon_url=message2.author.avatar_url) #hay que guardar autores para que esto funcione
+                    await message2.channel.send(embed=msg)
+                    ydl_opts = {}
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        song_info = ydl.extract_info(canciones[0], download=False)
+                        #print(song_info)
+                    OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+                    vc.play(discord.FFmpegPCMAudio(executable=exe, source=song_info["formats"][0]["url"], **OP))
+        except Exception:
+            pass
             
         
         if vc.is_playing() != False:
@@ -166,6 +187,13 @@ async def texto_a_link(link):
     link = f"https://youtu.be/{str(t)[41:-1]}"
     canciones.append(link)
     return pytube.YouTube(link) #para comprobar si una cancion esta puesta, al enviar el mensaje se comprueba, no todo el rato
+
+async def texto_a_linkf(link):
+    s = Search(link)
+    t = s.results[0]
+ 
+    link = f"https://youtu.be/{str(t)[41:-1]}"
+    return link
 
 
 async def adelantar_y_atrasar(message, canciones):
@@ -207,4 +235,4 @@ async def atrasar(message, canciones):
 
 
 
-bot.run("NzkyNzQ3ODQ3NzYxMDAyNTM2.X-iN9w.s4mprx-0p5SZdNvwXLinCg420_4")
+bot.run("NzkyNzQ3ODQ3NzYxMDAyNTM2.X-iN9w.r3AhEi0YUenvGe6gcSllESCh3bo")

656f7af17aee86298d8d5cd23f93c9f9985cf383
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-03-06 21:24:11 +0100

    Update bot_rework.py

--- bot_rework.py
+++ bot_rework.py
@@ -1,23 +1,23 @@
 from __future__ import unicode_literals
+import asyncio
 import discord
 from discord.utils import get
 from discord.ext import commands
 from discord import FFmpegOpusAudio
 from discord import app_commands
-
 import threading
 import time
 import pytube
-import youtube_dl
+import yt_dlp
 import socket
 import os
 import time
 import requests
 from tinytag import TinyTag
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
 from pytube import Search
+import typing
+
+tags = {}
 
 vc = ""
 lista = []
@@ -30,88 +30,212 @@ tree = app_commands.CommandTree(bot)
 
 class reproductor(discord.ui.View):
     parar : bool = True
-    
-    @discord.ui.button(label="Stop", style=discord.ButtonStyle.danger)
+    parar = True
+    @discord.ui.button(label="Desconectar", style=discord.ButtonStyle.danger)
+    async def desconectar(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await vc.disconnect()
+        await interaction.response.send_message("Desconectado", ephemeral=True)
+
+    @discord.ui.button(label="Stop/Resume", style=discord.ButtonStyle.blurple)
     async def parar(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.parar = True
         if self.parar == True:
-            vc.pause()
-            button.label = "Play"
-            self.parar = False
-            await interaction.response.send_message("Pausado", ephemeral=True)
-        else:
             vc.resume()
             button.label = "Stop"
+            self.parar = False
+            await interaction.response.send_message("Vuelto a poner", ephemeral=True)
+        else:
+            vc.pause()
+            button.label = "Play"
             self.parar = True
-            await interaction.response.edit_message(view=self)
-
+            await interaction.response.send_message("Pausado", ephemeral=True)
+    @discord.ui.button(label="Skip", style=discord.ButtonStyle.blurple)
+    async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
+        vc.stop()
+        if lista[0]:
+            del lista[0]
+        else:
+            interaction.response.send_message("No hay cancion a la que skipear")
+        interaction.response.send_message("Saltando...", ephemeral=True)
+        playback(interaction)
 
 @bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=792826492102377504))
+    global client
+    await tree.sync(guild=discord.Object(id=369922977690681345))
     print("uff")
     await bot.change_presence(activity=discord.Game(name="/play para poner canciones!"))
 
-@tree.command(name = "play", description= "Pon el nombre o el link de youtube de la cancion que quieras", guild=discord.Object(id=792826492102377504))
-async def play(interaction: discord.Interaction, link: str):
+@tree.command(name = "play", description= "Pon el nombre o el link de youtube de la cancion que quieras", guild=discord.Object(id=369922977690681345))
+async def play(interaction: discord.Interaction, cancion: str):
     global vc, lista, EXE
-    if "https:" in link:
-        info = pytube.YouTube(link)
+    if "https:" in cancion:
+        info = pytube.YouTube(cancion)
     else:
-        s = Search(link)
+        s = Search(cancion)
         s = s.results[0]
         info = pytube.YouTube(f"https://youtu.be/{str(s)[41:-1]}")
-        link = f"https://youtu.be/{str(s)[41:-1]}"
+        cancion = f"https://youtu.be/{str(s)[41:-1]}"
     if info.title == None:
         info.title = ""
-    if type(vc) != str:
-        if vc.is_playing() == True:
-            lista.append(link)
-            msg = discord.Embed(title= f"{info.title}", description= f"Se ha puesto en cola {info.title}, esta en el puesto {len(lista)}", url=link, color=interaction.user.color)
-            msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=msg)
+    lista.append(cancion)
+    if lista:
+        if type(vc) != str:
+            if vc.is_playing() == True:
+                lista.append(cancion)
+                msg = discord.Embed(title= f"{info.title}", description= f"Se ha puesto en cola {info.title}, esta en el puesto {len(lista)}", url=cancion, color=interaction.user.color)
+                msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                await interaction.response.send_message(embed=msg)
+            else:
+                msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=lista[0], color=interaction.user.color)
+                msg.set_thumbnail(url=info.thumbnail_url)
+                msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                await interaction.response.send_message(embed=msg, view=reproductor())
+                ydl_opts = {
+                'format': 'bestaudio/best',
+                'noplaylist':'True',
+                'outtmpl': 'song.%(ext)s',
+                'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+                }]}
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    song_info = ydl.extract_info(lista[0], download=False)
+                del lista[0]
+                OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+                vc.play(discord.FFmpegOpusAudio(executable=EXE, source=song_info['url'], **OP))
         else:
-            msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=lista[0], color=interaction.user.color)
-            msg.set_thumbnail(url=info.thumbnail_url)
-            msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=msg)
-            ydl_opts = {
-            'format': 'bestaudio/best',
-            'noplaylist':'True',
-            'outtmpl': 'song.%(ext)s',
-            'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-            }]}
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                song_info = ydl.extract_info(lista[0], download=False)
-            OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            vc.play(discord.FFmpegOpusAudio(executable=EXE, source=song_info["formats"][0]["url"], **OP))
+            if interaction.user.voice != None:
+                lista.append(cancion)
+                vc = await interaction.user.voice.channel.connect()
+                view = reproductor()
+                msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=lista[0], color=interaction.user.color)
+                msg.set_thumbnail(url=info.thumbnail_url)
+                msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                await interaction.response.send_message(embed=msg, view=view)
+                ydl_opts = {
+                'format': 'bestaudio/best',
+                'noplaylist':'True',
+                'outtmpl': 'song.%(ext)s',
+                'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+                }]}
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    song_info = ydl.extract_info(lista[0], download=False)
+                OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+                del lista[0]
+                vc.play(discord.FFmpegOpusAudio(executable=EXE, source=song_info['url'], **OP), after=lambda e: playback(interaction))
+            else:
+                msg = discord.Embed(title="Error", description="No estas en un canal de voz", color=0xe74c3c)
+
+@play.autocomplete('cancion')
+async def fruits_autocomplete(
+    interaction: discord.Interaction,
+    current: str
+) -> list[app_commands.Choice[str]]:
+    fruits = Search(current).completion_suggestions
+    print(type(fruits))
+    print(len(fruits))
+    return [
+        app_commands.Choice(name=fruit, value=fruit)
+        for fruit in fruits if current.lower() in fruit.lower()
+    ]
+
+def playback(interaction: discord.Interaction):
+    del lista[0]
+    cancion = lista[0]
+    if "https:" in cancion:
+        info = pytube.YouTube(cancion)
     else:
-        if interaction.user.voice != None:
-            lista.append(link)
-            vc = await interaction.user.voice.channel.connect()
-            view = reproductor()
-            msg = discord.Embed(title= f"{info.title}", description= f"Se esta reproduciendo {info.title}", url=lista[0], color=interaction.user.color)
-            msg.set_thumbnail(url=info.thumbnail_url)
-            msg.set_author(name= interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=msg, view=view)
-            ydl_opts = {
-            'format': 'bestaudio/best',
-            'noplaylist':'True',
-            'outtmpl': 'song.%(ext)s',
-            'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-            }]}
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                song_info = ydl.extract_info(lista[0], download=False)
-            OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            vc.play(discord.FFmpegOpusAudio(executable=EXE, source=song_info["formats"][0]["url"], **OP))
-        else:
-            msg = discord.Embed(title="Error", description="No estas en un canal de voz", color=0xe74c3c)
+        s = Search(cancion)
+        s = s.results[0]
+        info = pytube.YouTube(f"https://youtu.be/{str(s)[41:-1]}")
+        cancion = f"https://youtu.be/{str(s)[41:-1]}"
+    if info.title == None:
+        info.title = ""
+    #asyncio.run_coroutine_threadsafe(interaction.channel.send(embed=msg, view=view), asyncio.get_event_loop())
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'noplaylist':'True',
+    'outtmpl': 'song.%(ext)s',
+    'postprocessors': [{
+    'key': 'FFmpegExtractAudio',
+    'preferredcodec': 'mp3',
+    'preferredquality': '192',
+    }]}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        song_info = ydl.extract_info(lista[0], download=False)
+    del lista[0]
+    OP = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    vc.play(discord.FFmpegOpusAudio(executable=EXE, source=song_info['url'], **OP), after=lambda e: playback(interaction))
+
+
+@tree.command(name = "set-fc", description= "Pon los gamertags que quieras de la lista!", guild=discord.Object(id=369922977690681345))
+@app_commands.describe(n3ds="Pon tu codigo de amigo de 3ds. Ejemplo: 4141-9637-9341")
+@app_commands.describe(switch="Pon tu codigo de amigo de switch. Ejemplo: SW-2809-4574-2883")
+@app_commands.describe(pogo="Pon tu codigo de amigo de pokemon go. Ejemplo: 9387-7346-1802")
+@app_commands.describe(masters="Pon tu codigo de amigo de pokemon masters. Ejemplo: 9181-3142-9313-6046")
+@app_commands.describe(home="Pon tu codigo de amigo de pokemon home. Ejemplo: LKMWQXNKLZLZ")
+@app_commands.describe(unite="Pon tu codigo de amigo de pokemon unite. Ejemplo: Y1292Q6")
+async def setfc(interaction: discord.Interaction, n3ds: typing.Optional[str], switch: typing.Optional[str], pogo: typing.Optional[str], masters: typing.Optional[str], home: typing.Optional[str], unite: typing.Optional[str]):
+    global tags
+    gamertags = []
+    if (n3ds):
+        gamertags.append({"n3ds":n3ds})
+    if (switch):
+        gamertags.append({"switch":switch})
+    if (pogo):
+        gamertags.append({"pogo":pogo})
+    if (masters):
+        gamertags.append({"masters":masters})
+    if (home):
+        gamertags.append({"home":home})
+    if (unite):
+        gamertags.append({"unite":unite})
+
+    tags.update({interaction.user: gamertags})
+    msg = discord.Embed(title="Hecho!", description="Se ha establecido correctamente tu código de amigo.", color=0x2ecc71)
+    print(tags)
+    await interaction.response.send_message(embed=msg, ephemeral=True)
+
+@tree.command(name = "fc", description= "Muestra los gamertags de algiuen o si no pones argumentos son los tuyos propios", guild=discord.Object(id=369922977690681345))
+@app_commands.describe(usuario="Pinguea el usuario del cual quieres ver los tags, sino pones nada pone los tuyos")
+async def fc(interaction: discord.Interaction, usuario: typing.Optional[str]):
+    global tags
+    if usuario:
+        int(usuario[2:-1])
+        user = bot.get_user(int(usuario[2:-1]))
+    else:
+        user = interaction.user
+    desc = ""
+    try:
+        datos = tags[user]
+    except Exception:
+        msg = discord.Embed(title="Error", description="La persona que has mencionado no tiene puestos sus tags!", color=0xe74c3c)
+        await interaction.response.send_message(embed=msg)
+        return
+    for i in datos:
+        key = i.keys()
+        for a in key:
+            desc = desc + f"**{a.upper()}**\n{i[a]}\n\n"
+    if desc == "":
+        msg = discord.Embed(title="Error", description="La persona que has mencionado no tiene puestos sus tags!")
+    else:
+        msg = discord.Embed(title=f"Codigos de amigo de {user.display_name}", description=desc, color=user.color)
+    await interaction.response.send_message(embed=msg)
+
+@tree.command(name = "del-fc", description= "Borra tu perfil de codigos de amigo", guild=discord.Object(id=369922977690681345))
+async def delete(interaction: discord.Interaction):
+    global tags
+    user = interaction.user
+    try:
+        del tags[user]
+        msg = discord.Embed(title="Hecho!", description="Se ha borrado correctamente tu perfil", color=0x2ecc71)
+    except Exception:
+        msg = discord.Embed(title="Error", description="No tienes tus tags puestos", color=0xe74c3c)
+    await interaction.response.send_message(embed=msg, ephemeral=true)
 
 bot.run("")
 
